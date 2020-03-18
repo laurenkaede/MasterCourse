cd4a4421c517a843a2a09a795428989ca7520567
Lauren Moor <laurenkaede@gmail.com>
2020-03-18 16:50:22 +0000

    end of day

--- Week 4/Weds 18th March/mastercoding-4-JS-DOM-3-JS-DOM-dicegame1-starterfile 2/script.js
+++ Week 4/Weds 18th March/mastercoding-4-JS-DOM-3-JS-DOM-dicegame1-starterfile 2/script.js
@@ -1,51 +1,34 @@
-// button.addEventListener("click", () => {
-//     image.src = input.value;
-// })
-
-// function display_random_image() {
-// let theImages = ['./img./dice1.png', './img./dice2.png', './img./dice3.png', './img./dice4.png', './img./dice5.png', './img./dice6.png'];
+let scores, roundScore, activePlayer, gamePlaying;
 
 function display_random_image() {
     let theImages = [{
-        src: './img./dice1.png',
-        width: "250",
-        height: "250"
+        src: 'dice1.png'
     }, {
-        src: './img./dice2.png',
-        width: "250",
-        height: "250"
+        src: 'dice2.png',
     }, {
-        src: './img./dice3.png',
-        width: "250",
-        height: "250"
+        src: 'dice3.png',
     }, {
-        src: './img./dice4.png',
-        width: "250",
-        height: "250"
+        src: 'dice4.png',
     }, {
-        src: './img./dice5.png',
-        width: "250",
-        height: "250"
+        src: 'dice5.png',
     }, {
-        src: './img./dice6.png',
-        width: "250",
-        height: "250"
+        src: 'dice6.png',
     }];
 
-    let preBuffer = [];
+    let diceImages = [];
     for (let i = 0, j = theImages.length; i < j; i++) {
-        preBuffer[i] = new Image();
-        preBuffer[i].src = theImages[i].src;
-        preBuffer[i].width = theImages[i].width;
-        preBuffer[i].height = theImages[i].height;
+        diceImages[i] = new Image();
+        diceImages[i].src = theImages[i].src;
+        diceImages[i].width = theImages[i].width;
+        diceImages[i].height = theImages[i].height;
     }
 
     function getRandomNum(min, max) {
-        imn = Math.floor(Math.random() * (max - min + 1)) + min;
-        return preBuffer[imn];
+        img = Math.floor(Math.random() * (max - min + 1)) + min;
+        return diceImages[img];
     }
 
-    let newImage = getRandomNum(0, preBuffer.length - 1);
+    let newImage = getRandomNum(0, diceImages.length - 1);
 
     let images = document.getElementsByTagName('img');
     let l = images.length;
@@ -56,8 +39,16 @@ function display_random_image() {
     document.body.appendChild(newImage);
 }
 
-
-// while (images.length !== 0) {
-//     let index = Math.floor(Math.random() * images.length);
-//     $('<img src="images/large/' + images.splice(index, 1) + '">').appendTo('#images');
-// }
+// let score = [{
+//     src: 'dice1.png' = "1"
+// }, {
+//     src: 'dice2.png' = "2",
+// }, {
+//     src: 'dice3.png' = "3",
+// }, {
+//     src: 'dice4.png' = "4",
+// }, {
+//     src: 'dice5.png' = "5",
+// }, {
+//     src: 'dice6.png' = "6",
+// }];
